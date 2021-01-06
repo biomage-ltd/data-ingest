160c7e49a85bdf43c36da76751fead58ebf83fb9
NaomiBreslin <32261329+NaomiBreslin@users.noreply.github.com>
2021-01-06 21:50:49 +0000

    Update 3-preproc.r
    Seurat pipeline.
    Check comments for more information

--- src/3-preproc.r
+++ src/3-preproc.r
@@ -1,8 +1,6 @@
-library(pagoda2)
-library(conos)
+library(Seurat)
 library(Matrix)
 require(data.table)
-require(conos)
 library(gprofiler2)
 
 get_real_cells <- function(data, score_filter = 0.2) {
@@ -29,52 +27,109 @@ message("removing doublets...")
 data$filtered <- data$filtered[, real_barcodes]
 
 
+message("Creating Seurat Object...")
+data <- Seurat::CreateSeuratObject(data$filtered, assay='RNA', min.cells=3, min.features=200)
+
+#Below are QC plots which are exported as pdf files. Uncomment to look at the qc plots.
+
+#pdf("/output/violin-pl.pdf")
+#VlnPlot(data, features = c("nFeature_RNA", "nCount_RNA", ncol=2))
+#dev.off()
+#pdf("/output/scatter-pl.pdf")
+#FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+#dev.off()
+
+#Using Seurat we can subset data. We can subset based on nUMI, nFeature and mito genes. We have filtered in 1.preproc.r so subset was not used. 
+
+#Uncomment the code below to remove mito genes. 
+#message("Subset and remove mito genes...")
+#data[["percent.mt"]] <- PercentageFeatureSet(data, pattern = "^MT-")
+
+#In order to determine the filter parameters for mito genes we would need to plot them on either a violin plot or a scatter plot. Please refer to the plots above.
+
+#Remove by subsetting... or look under ScaleData below for regressing out mito genes.
+#data <- subset(data, subset = percent.mt < 5)
+
 message('finding genome annotations for genes...')
 config <- RJSONIO::fromJSON("/input/meta.json")
 organism <- config$organism
 
 annotations <- gprofiler2::gconvert(
-    query = rownames(data$filtered), organism = organism, target="ENSG", mthreshold = Inf, filter_na = FALSE
-)
+    query = rownames(data), organism = organism, target="ENSG", mthreshold = Inf, filter_na = FALSE)
 
-message("creating pagoda2 object...")
-data <- Pagoda2$new(data$filtered)
-data$misc$gene_annotations <- annotations
+message("Storing annotations...")
+Misc(data, slot="annotations")
+data@misc$annotations <- annotations
 
 message("variance normalization...")
-data$adjustVariance(plot = F, gam.k = 10)
+data <- Seurat::NormalizeData(data, normalization.method = "LogNormalize", scale.factor = 10000) #default normalize method (check rdocs for other methods)
+data <-Seurat::FindVariableFeatures(data, selection.method = "vst", nfeatures = 3e3) #default method for finding variable features (check rdocs for other methods)
 
-# we're taking 3000 top overdispersed genes
-# (a lot more than the number that is significantly overdispersed)
+#feature scaling (takes a long time if you decide to scale this way)
+#all.genes <- rownames(data)
+#data <- Seurat::ScaleData(data, features=all.genes)
+
+data<-Seurat::ScaleData(data)
+#we can additionally regress out the mitochondrial genes
+#data <-Seurat::ScaleData(data, vars.to.regress= "percent.mt", verbose=FALSE)
+
+
 message("computing PCA reduction...")
-data$calculatePcaReduction(nPcs = 50, n.odgenes = 3e3)
+data<-Seurat::RunPCA(data, npcs = 50, features = VariableFeatures(object=data), verbose=FALSE)
 
 message("creating kNN graph...")
-data$makeKnnGraph(
-    k = 20, type = "PCA", center = T,
-    distance = "cosine", weight.type = "none"
-)
+data <- FindNeighbors(data) #default method
 
-message("computing leiden clusters...")
-data$getKnnClusters(method = conos::leiden.community, type = "PCA")
+#example of cosine method
+#data<- FindNeighbors(
+ #   data, k.param = 20, 
+  #  annoy.metric = "cosine", verbose=FALSE)
+
+message("computing louvain clusters...")
+data <- FindClusters(data, resolution=0.5) #default method (Louvain clusters)
+
+#clustering algorith=4 is computing Leiden Clusters. 
+#data <- FindClusters(data,
+ #   method = "igraph", algorithm = 4, verbose=FALSE)
+
+vars <-HVFInfo(object = data[["RNA"]], selection.method = 'vst')
+
+vars <- vars[, 'variance.standardized', drop=FALSE]
+
+message("Running embedding")
+data <- RunUMAP(data, reduction='pca', dims = 1:10)
+
+pdf("/output/umap.pdf")
+DimPlot(data, reduction = "umap")
+dev.off()
+
+#We can also create a tsne plot
+#message("Running embedding")
+#data <- RunTSNE(data, reduction='pca', dims = 1:10)
+#pdf("/output/tsne.pdf")
+#DimPlot(data, reduction = "tsne")
+#dev.off()
+
+message("saving R object...")
+saveRDS(data, file = "/output/experiment.rds", compress = FALSE)
 
 message("Saving gene and cell data...")
 write.table(
-    data$misc$varinfo[c("qv")],
+    vars,
     file = "/output/r-out-dispersions.csv",
     sep = ",",
     col.names = F
 )
 
 write.table(
-    rownames(data$counts),
+    colnames(data),
     file = "/output/r-out-cells.csv",
     quote = F, col.names = F, row.names = F,
     sep = "\t"
 )
 
 write.table(
-    data$misc$gene_annotations,
+    data@misc[["annotations"]],
     file = "/output/r-out-annotations.csv",
     quote = F, col.names = F, row.names = F,
     sep = "\t"
@@ -84,13 +139,13 @@ message("saving R object...")
 saveRDS(data, file = "/output/experiment.rds", compress = FALSE)
 
 message("saving normalized matrix...")
-Matrix::writeMM(data$counts, file = "/output/r-out-normalized.mtx")
+Matrix::writeMM(t(data@assays$RNA@data), file = "/output/r-out-normalized.mtx")
 
 message("saving raw matrix...")
-Matrix::writeMM(
-    data$misc$rawCounts[
-        rownames(data$counts),
-        colnames(data$counts)
-    ],
+Matrix::writeMM(t(
+    data@assays$RNA@counts[
+        rownames(data@assays$RNA@data),
+        colnames(data@assays$RNA@data)
+    ]),
     file = "/output/r-out-raw.mtx"
 )
