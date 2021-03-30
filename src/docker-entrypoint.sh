8a4bc7abf3ee6fb9062efc6254f523bbd1850127
Juanlu <juanlu@biomage.net>
2021-03-30 21:28:49 +0200

    Fix file name

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-#python3 -u /data-ingest/src/pre_run_test.py
-#Rscript /data-ingest/src/1_Preproc.r ${INPUT_TYPE-10x}
-#Rscript /data-ingest/src/2-1_Compute-metris_emptyDrops.r
-#python3 -u /data-ingest/src/2-2_Compute-metrics_scrublet.py
-#Rscript /data-ingest/src/3_Seurat.r
+python3 -u /data-ingest/src/pre_run_test.py
+Rscript /data-ingest/src/1_Preproc.r ${INPUT_TYPE-10x}
+Rscript /data-ingest/src/2-1_Compute-metrics_emptyDrops.r
+python3 -u /data-ingest/src/2-2_Compute-metrics_scrublet.py
+Rscript /data-ingest/src/3_Seurat.r
 Rscript /data-ingest/src/4_Prepare_experiment.r
-python3 -u /data-ingest/src/5_upload-to-aws.py
+python3 -u /data-ingest/src/5_Upload-to-aws.py
