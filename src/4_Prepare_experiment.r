7f51b1eff84fd98f0bea9b48e671b6053b6ed378
ogibson <oliver@biomage.net>
2021-04-06 15:55:03 -0300

    Removed enabled from integration-embedding

--- src/4_Prepare_experiment.r
+++ src/4_Prepare_experiment.r
@@ -227,14 +227,14 @@ config.doubletScores <- list(enabled="true", auto="true",
 
 # BE CAREFUL! The method is based on config.json. For multisample only seuratv4, for unisample LogNormalize
 identified.method <- ifelse(length(samples)==1, "unisample", "seuratv4")
-config.dataIntegration <- list(enabled="true", auto="true", 
+config.dataIntegration <- list(auto="true", 
     dataIntegration = list( method = identified.method , 
                         methodSettings = list(seuratv4=list(numGenes=2000, normalisation="logNormalize"), 
                                             unisample=list(numGenes=2000, normalisation="logNormalize"))),
     dimensionalityReduction = list(method = "rpca", numPCs = 30, excludeGeneCategories = c())
 )
 
-config.configureEmbedding <- list(enabled="true", auto="true", 
+config.configureEmbedding <- list(auto="true", 
     embeddingSettings = list(method = "umap", methodSettings = list(
                                 umap = list(minimumDistance=0.3, distanceMetric="euclidean"), 
                                 tsne = list(perplexity=min(30, ncol(seurat_obj)/100), learningRate=max(200, ncol(seurat_obj)/12))
