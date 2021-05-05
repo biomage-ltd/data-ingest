ef88f6c25c64e15d4dbca73d70a5ff7ee804c73a
Alex Pickering <alexvpickering@gmail.com>
2021-05-05 15:18:14 -0700

    add number of filtered experiments to exp_meta.csv

--- src/wrapper/add_exp_meta.R
+++ src/wrapper/add_exp_meta.R
@@ -1,12 +1,15 @@
 meta <- jsonlite::fromJSON('./input/meta.json', simplifyVector = TRUE)
 experiment_id <- readLines('./output/experiment_id.txt', warn = FALSE)
 
+n_filtered <- length(list.files('./output', 'pre-emptydrops'))
+
 res <- data.frame(
   experiment_id,
   experiment_name = meta$name,
   organism = meta$organism,
   url = sprintf('https://scp.biomage.net/experiments/%s', experiment_id),
-  env = Sys.getenv('CLUSTER_ENV', 'staging')
+  env = Sys.getenv('CLUSTER_ENV', 'staging'),
+  n_filtered
 )
 
 write.table(res, "exp_meta.csv", sep = ",",
